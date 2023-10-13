52484924786969cb6b761ddd695949d82a714621
James Clarke <jraclarke@hotmail.com>
2023-10-13 10:59:06 +0100

    Add download convenience files for SD videos.

---
+++ SceneGraph/Bubble/StreetDrone/Cameras/Rear/Video/download_all_sd_rearcam_videos.sh
@@ -0,0 +1,2 @@
+wget https://hillyfields.s3.amazonaws.com/releases/scenegraph/bubble/StreetDrone/Cameras/Front/Video/STREETDRONE.FRONTCAM_VIDEO.2023-04-26T13%3A37%3A36.530_obfuscated.mp4 
+wget https://hillyfields.s3.amazonaws.com/releases/scenegraph/bubble/StreetDrone/Cameras/Front/Video/STREETDRONE.FRONTCAM_VIDEO.2023-04-26T14%3A03%3A36.504_obfuscated.mp4 
